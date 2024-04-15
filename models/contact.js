b9be191e67b1d430e0286272368962d7bec9abbf
Kevelaz <kevinvelazquez925@gmail.com>
2024-04-15 16:42:19 -0400

    nodemailer tested and ready

--- models/contact.js
+++ models/contact.js
@@ -1,6 +1,6 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const contactSchema = new Schema ({
   name: {
